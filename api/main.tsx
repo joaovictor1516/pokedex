90b92438b8426413e6b5cf57893b14554195718f
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-01-10 16:52:46 -0300

    arrumando a chamada de erro

--- api/main.tsx
+++ api/main.tsx
@@ -3,11 +3,11 @@ import { PokemonClient }  from "pokenode-ts";
 const PokedexApi = new PokemonClient();
 
 export function searchPokemon(name: string){
-  PokedexApi.getPokemonByName(name) // with Promise
+  PokedexApi.getPokemonByName(name)
     .then((response) => {
       console.log(response);
     })
     .catch((error) => {
-      console.log('There was an ERROR: ', error);
+      throw new Error(error);
     });
 }
