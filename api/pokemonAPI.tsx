5bc2cee23d73aa18e33f0e8282d46ab5eadb064e
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-01-13 22:05:08 -0300

    alteracao do nome do arquivo para que seja mais facil de saber sobre o que se trata esta api e adicao da funcao de buscar o pokemon via id

---
+++ api/pokemonAPI.tsx
@@ -0,0 +1,32 @@
+import { PokemonClient }  from "pokenode-ts";
+
+const PokemonApi = new PokemonClient();
+
+export type searchPokemonByName = {
+  id: number,
+  name: string
+};
+
+export function searchPokemonByName(name: string){
+  PokemonApi.getPokemonByName(name)
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+};
+
+export function searchPokemonById(id: number){
+  PokemonApi.getPokemonById(id)
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    throw new Error(error);
+  })
+};
+
+export function searchPockemonGroup(name: string){
+
+}
