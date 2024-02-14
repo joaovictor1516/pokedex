8ba61c4529aaa78d4e50bd08b612d29d9cd858ed
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-02-14 16:56:34 -0300

    arrumacao do layout da pagina

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="favicon.ico" sizes="any" />
       </head>
-      <body>{children}</body>
+      <body className="bg-stone-200">{children}</body>
     </html>
   )
 }
