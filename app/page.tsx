4fcbdc89b13090e1863a3db70fac125ce48f92f9
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-02-15 12:34:05 -0300

    adicao do componente header

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,46 @@
 "use client";
-import React, {ChangeEvent, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import 'tailwindcss/tailwind.css';
 import Principal from './components/pokemon';
+import Header from './components/header';
 
 export default function Home(){
   const [pokemons, setPokemons] = useState("");
+  const [data, setData] = useState([]);
 
-function textSearch(value: ChangeEvent<HTMLInputElement>){
-  const text = value.target.value;
-  console.log(text);
+  useEffect(() => {
+    takeData();
+  }, []);  
+
+  const takeData = async () => {
+    try{
+      const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+      const results = response.data.results;
+      setData(results);
+      setPokemons(results.name);
+    } catch(error){
+      console.error(`Error: ${error}`);
+    }
+  }
+
+function handleTextSearch(content: string){
+  const text = content;
   setPokemons(text);
 }
-
   
   return (
     <>
     <header>
-      <input type="text" 
-        onChange={textSearch}
-      />
+      <Header searchFunction={handleTextSearch}/>
     </header>
 
-    <main>
-      <Principal id={2} name={pokemons}/>
-    </main>
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+      {data.map((pokemon, index) => (
+        <Principal key={index} name={pokemon.name}/>
+      ))}
+      <Principal name={pokemons}/>
+    </div>
     </>
   )
 }
