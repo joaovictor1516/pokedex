5309357d58f0d7964ed0dca70e9ee04d779382ad
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-02-17 17:42:59 -0300

    estilizando o componente

--- app/components/pokemon.tsx
+++ app/components/pokemon.tsx
@@ -7,8 +7,9 @@ interface Pokemon{
 
 export default function Principal(props: Readonly<Pokemon>){
     return(
-        <div className="flex  border-solid border-red-600 border-2 text-center">
-            <span>{props.name}</span>
+        <div className="flex flex-1 flex-col gap-y-0.5 border-solid border-red-600 border-2 text-center rounded-md w-auto max-w-md">
+            <span className="w-auto">{props.name}</span>
+            <p className="w-auto">teste</p>
         </div>
     )
 }
