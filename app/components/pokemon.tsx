1e113abb4262e016821ce3f854fa900222553d60
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-02-19 20:39:23 -0300

    refatoracao do codigo e tentativa mal sucedida de correcao do bug

--- app/components/pokemon.tsx
+++ app/components/pokemon.tsx
@@ -1,15 +1,28 @@
 "use client";
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+import { Ability } from '../interfaces/interfacesAPI';
 interface Pokemon{
     name: string;
-    ability: string;
+    abilities: Ability[];
 }
 
 export default function Principal(props: Readonly<Pokemon>){
+
+    console.log("Dados: ", props);
     return(
-        <div className="flex flex-1 flex-col gap-y-0.5 border-solid border-red-600 border-2 text-center rounded-md w-auto max-w-md">
-            <span className="w-auto">{props.name}</span>
-            <p className="w-auto">teste</p>
+        <div className="flex flex-1 flex-col gap-y-0.5 border-solid border-red-600 border-2 justify-items-center rounded-md w-28 max-w-md">
+        
+        <span className="text-center">
+            {props.name}
+        </span>
+        
+        <div className="flex flex-col flex-1 items-center">
+            {props.abilities.map((ability, id) => (
+                <span key={id} className="text-center">
+                    {ability.name},
+                </span>
+            ))}
+        </div>
         </div>
     )
 }
