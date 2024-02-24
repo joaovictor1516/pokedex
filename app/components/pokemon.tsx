6e7ff24c780f4434f210dd65ff9670bc304e38c1
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-02-23 23:29:56 -0300

    adicao do dialog para mostrar mais detalhes do pokemon selecionado

--- app/components/pokemon.tsx
+++ app/components/pokemon.tsx
@@ -1,5 +1,6 @@
 "use client";
 import 'tailwindcss/tailwind.css';
+import * as Dialog from "@radix-ui/react-dialog";
 import { Pokemon } from '../interfaces/interfacesAPI';
 
 
@@ -8,21 +9,28 @@ export default function Principal(props: Readonly<Pokemon>){
 
     console.log("Dados: ", props);
     return(
-        <div className="flex flex-1 flex-col gap-y-0.5 border-solid border-red-600 border-2 justify-items-center rounded-md w-28 max-w-md">
-        
-        <span className="text-center">
-            {props.name}
-        </span>
-        
-        <div className="flex flex-col flex-1 items-center">
-            {props.abilities.map((abilityItem, id) => (
-                
-                <span key={id} className="text-center">
-                    {abilityItem.ability.name}
+        <Dialog.Root>
+            <Dialog.Trigger className="flex flex-1 flex-col gap-y-0.5 border-solid border-red-600 border-2 justify-items-center rounded-md w-28 max-w-md">
+            
+                <span className="text-center">
+                    {props.name}
                 </span>
-                )
-            )}
-        </div>
-        </div>
+                
+                <div className="flex flex-col flex-1 items-center">
+                    {props.abilities.map((abilityItem, id) => (
+                
+                        <span key={id} className="text-center font-sans font-normal">
+                            {abilityItem.ability.name}
+                        </span>
+                        )
+                    )}
+                </div>
+            </Dialog.Trigger>
+
+            <Dialog.Content className="fixed w-10 z-0 bg-red-400 text-black">
+                <div>Teste</div>
+            </Dialog.Content>
+
+        </Dialog.Root>
     )
 }
