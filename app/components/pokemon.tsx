f001f7f889150bea4e2ef6f1a7d57ad18411540c
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-02-25 23:07:43 -0300

    arrumando os dados do componente

--- app/components/pokemon.tsx
+++ app/components/pokemon.tsx
@@ -7,16 +7,17 @@ import { Pokemon } from '../interfaces/interfacesAPI';
 
 export default function Principal(props: Readonly<Pokemon>){
 
-    console.log("Dados: ", props);
     return(
         <Dialog.Root>
-            <Dialog.Trigger className="flex flex-1 flex-col gap-y-0.5 border-solid border-red-600 border-2 justify-items-center rounded-md w-28 max-w-md text-center">
+            <Dialog.Trigger 
+            className="flex flex-1 flex-col gap-y-0.5 border-solid border-red-600 border-2 justify-items-center rounded-md h-32 w-28 md:w-52 md:h-56 text-center">
             
                 <span className="">
                     {props.name}
                 </span>
                 
-                <div className="flex flex-col flex-1 items-center">
+                <div 
+                className="flex flex-row flex-1 items-center gap-x-1">
                     {props.abilities.map((abilityItem, id) => (
                 
                         <span key={id} className="font-sans font-normal">
@@ -30,7 +31,8 @@ export default function Principal(props: Readonly<Pokemon>){
             <Dialog.Portal>
                 <Dialog.Overlay className='inset-0 fixed bg-black/50'/>
 
-                <Dialog.Content className="inset-0 fixed rounded-md translate-x-1/2 w-1/2 h-1/2 bg-red-400 border-solid text-black">
+                <Dialog.Content 
+                className="inset-0 fixed rounded-md translate-x-1/2 w-1/2 h-1/2 bg-red-400 border-solid text-black">
                     <div className="flex flex-col text-center">
 
                         <h1 className="">
@@ -38,8 +40,12 @@ export default function Principal(props: Readonly<Pokemon>){
                         </h1>
 
                         <span className="">
-
+                            
                         </span>
+
+                        <div className="">
+
+                        </div>
                     </div>
                 </Dialog.Content>
             </Dialog.Portal>
