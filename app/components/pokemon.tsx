b8c3feddef069e5c17ad75a6a7c8c02599432ee1
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-02-27 16:40:17 -0300

    centralizacao dos elementos e criacao da funcao responsavel por colorir o fundo do texto do tipo do pokemon

--- app/components/pokemon.tsx
+++ app/components/pokemon.tsx
@@ -2,22 +2,44 @@
 import 'tailwindcss/tailwind.css';
 import * as Dialog from "@radix-ui/react-dialog";
 import { Pokemon } from '../interfaces/interfacesAPI';
-
-
+import { useEffect, useState } from 'react';
 
 export default function Principal(props: Readonly<Pokemon>){
+    const [color, setColor] = useState("white");
+
+    useEffect(() => {
+        const changeColor = () => {
+            const objColors:{[key: string]: string} = {
+                fire: "bg-red-400",
+                normal: "bg-neutral-400",
+                rock: "bg-gray-400",
+                water: "bg-blue-400",
+                grass: "bg-green-400",
+                flying: "bg-gradient-to-b from-sky-400 to-gray-400",
+                poison: "bg-purple-400"
+            };
+    
+            props.types.forEach((typeItem) => {
+                if(objColors[typeItem.type.name]){
+                    setColor(objColors[typeItem.type.name]);
+                }
+            })
+        };
+
+        changeColor();
+    },[props.types]);
 
     return(
         <Dialog.Root>
             <Dialog.Trigger 
-            className="flex flex-1 flex-col gap-y-0.5 border-solid border-red-600 border-2 justify-items-center rounded-md h-32 w-28 md:w-52 md:h-56 text-center">
+            className="flex flex-1 flex-col gap-y-0.5 border-solid border-red-600 border-2 justify-center content-center items-center text-center rounded-md h-32 w-28 md:w-52 md:h-56">
             
                 <span className="">
                     {props.name}
                 </span>
                 
                 <div 
-                className="flex flex-row flex-1 items-center gap-x-1">
+                className="flex flex-row flex-1 flex-wrap gap-x-1 justify-center content-center text-center">
                     {props.abilities.map((abilityItem, id) => (
                 
                         <span key={id} className="font-sans font-normal">
@@ -25,6 +47,16 @@ export default function Principal(props: Readonly<Pokemon>){
                         </span>
                         )
                     )}
+                </div>
+
+                <div 
+                className="flex flex-row flex-1 flex-wrap gap-x-1 justify-center content-center text-center">
+                    {props.types.map((typeItem, id) => (
+                        <span key={id}
+                            className={`font-sans font-normal ${color} p-1 rounded`}>
+                            {typeItem.type.name}
+                        </span>
+                    ))}
                 </div>
             </Dialog.Trigger>
 
