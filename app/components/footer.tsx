2f05152510d1e019e61cc4f1cf1d47efcef64c14
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-03-01 15:16:14 -0300

    dicao das imagens da tecnologias usadas

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -11,33 +11,61 @@ export default function Footer(){
                     <a href="https://github.com/joaovictor1516">
                         <img src="/assets/githubLogo.png"
                             alt="Imagem de uma silhueta branca de de um gato em um fundo preto arredondado"
-                            className="w-8 h-8"/>
-                        Meu github
+                            className="w-6 h-6"/>
+                        <p className="">
+                            Meu github
+                        </p>
                     </a>
                 </li>
                 <li className="flex flex-row justify-center">
                     <a href="https://www.linkedin.com/in/joao-victor-campos-souza-oliveira/">
                         <img src="/assets/linkedinLogo.png"
                             alt="As litras in na cor branca em um fundo azul quadrado com as bordas arredondadas"
-                            className="w-8 h-8"/>
-                        Meu linkedin
+                            className="w-6 h-6"/>
+                        <p className="">
+                            Meu linkedin
+                        </p>
                     </a>
                 </li>
             </ul>
             
             <ul className="flex flex-col w-40">
                 <h4>
-                    Texnologias usadas
+                    Tecnologias usadas:
                 </h4>
 
                 <li className="flex flex-row justify-center">
-                    <p>Next.JS</p>
+                    <img src="/assets/nextjsLogo.png" 
+                    alt="A letra 'n' escrita em maiusculo no fundo arredondado preto"
+                    className=""/>
+                    <p className="">
+                        Next.JS
+                    </p>
                 </li>
                 <li className="flex flex-row justify-center">
-                    <p>PokeAPI</p>
+                    <img src="/assets/pokeapiLogo.png" 
+                    alt="Pokeapi escrito em maiusculo na cor as lentras na cor amarela e com as bardas em azul"
+                    className=""/>
+                    <p className="">
+                        PokeAPI
+                    </p>
                 </li>
                 <li className="flex flex-row justify-center">
-                    <p></p>
+                    <img src="/assets/tailwindLogo" 
+                    alt="Logo do tailwind.css"
+                    className=""/>
+                    <p className="">
+                        Tailwind.CSS
+                    </p>
+                </li>
+
+                <li className="">
+                    <img src="/assets/typescriptLogo" 
+                    alt="As letras 't' e 's' em  maiusculo na cor branca localuizadas no canto inferior esquerdo de um quadrado em azul"
+                    className=""/>
+                    <p className="">
+                        TypeScript
+                    </p>
                 </li>
             </ul>
         </div>
