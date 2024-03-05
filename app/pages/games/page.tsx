146e054fcddfec6b6f11f9748b087f315ab0f6bf
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-03-05 08:40:19 -0300

    adicao da forma correta da rota games

---
+++ app/pages/games/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import "tailwindcss/tailwind.css";
+import { PokemonList } from "../../interfaces/interfacesAPI";
+
+export default function Games(props: PokemonList){
+    return(
+        <p>{props.name}</p>
+    )
+}
