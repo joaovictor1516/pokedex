95cb14768ef75160917ed92883b0e9db9d0dee89
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-03-05 08:39:22 -0300

    altercoes na pasta pages

--- app/pages/games.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import "tailwindcss/tailwind.css";
-import { PokemonList } from "../interfaces/interfacesAPI";
-
-export default function Games(props: PokemonList){
-    return(
-        <p>{props.name}</p>
-    )
-}
