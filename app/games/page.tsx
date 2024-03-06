51b61237eadf94f2d1eff84c1ef077f62e64809a
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-03-06 11:26:32 -0300

    correcao do bug da busca na pagina games

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -41,7 +41,7 @@ export default function ShowGames(){
 
     useEffect(() => {
         takeGameSearch();
-    }, []);
+    }, [textSearch]);
 
     return(
         <RootLayout searchFunction={handleSearch}>
