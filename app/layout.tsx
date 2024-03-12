9ce530e96d1155efa2e584105798f810238e303d
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-03-12 13:27:25 -0300

    alteracao do faicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        <link rel="icon" href="favicon.ico" sizes="any"/>
+        <link rel="icon" href="/assets/pokeBola.png" type="png" sizes="16x16"/>
       </head>
       <body className="bg-stone-200 text-black">
         {children}
