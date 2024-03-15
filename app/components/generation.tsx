93b6aa9987c652fe7e18100ce2264f0849cd9b47
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-03-15 18:54:47 -0300

    inicio da estruturacao do componente responsavel pelas geracoes dos pokemon

--- app/components/generation.tsx
+++ app/components/generation.tsx
@@ -2,5 +2,11 @@
 import { PokemonGeneration } from "../interfaces/interfacesAPI";
 
 export default function Generations(props: Readonly<PokemonGeneration>){
-    
+    return(
+        <div className="">
+            <h3 className="">
+                {props.name}
+            </h3>
+        </div>
+    )
 }
