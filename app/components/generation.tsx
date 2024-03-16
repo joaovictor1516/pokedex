2c34b059595dcb09f01748dbb0ae99b0ca257f45
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-03-16 14:26:46 -0300

    comecando a melhorar a aprencia do componente das geracoes

--- app/components/generation.tsx
+++ app/components/generation.tsx
@@ -3,22 +3,22 @@ import { PokemonGeneration } from "../interfaces/interfacesAPI";
 
 export default function Generations(props: Readonly<PokemonGeneration>){
     return(
-        <div className="">
-            <h1 className="">
+        <div className="flex flex-1 flex-col justify-center content-center">
+            <h1 className="text-center font-semibold md:font-bold">
                 {props.name}
             </h1>
 
-            <span className="">
+            <span className="text-center font-thin md:font-semibold">
                 Types:
             </span>
 
             {props.types !== undefined && props.types.length > 0 ? (
                 <div className="">
                     {props.types.map((type, id) => (
-                        <span className="" 
+                        <p className="" 
                             key={id}>
                                 {type.name[0].toUpperCase().concat(type.name.slice(1))}
-                        </span>
+                        </p>
                     ))}
             </div>
             ) : (
