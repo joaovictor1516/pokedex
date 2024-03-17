f7551792008956b11ac93fa808f2938135933b3f
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-03-17 17:37:46 -0300

    mais algumas alteracoes no css

--- app/components/generation.tsx
+++ app/components/generation.tsx
@@ -37,9 +37,9 @@ export default function Generations(props: Readonly<PokemonGeneration>){
             <Dialog.Portal>
                 <Dialog.Overlay className="inset-0 fixed bg-black/50"/>
 
-                <Dialog.Content className="fixed inset-0 xl:top-1/2 md:left-1/2 md:-translate-x-1/2 xl:-translate-y-1/2 flex flex-col w-full xl:max-w-[640px] xl:h-[85vh] bg-red-400 border-solid xl:rounded-md text-black md:overflow-y-scroll  xl:overflow-auto">
+                <Dialog.Content className="fixed inset-0 xl:top-1/2 md:left-1/2 md:-translate-x-1/2 xl:-translate-y-1/2 flex flex-col w-full xl:max-w-[640px] xl:h-[85vh] bg-red-400 border-solid xl:rounded-md text-black md:overflow-y-scroll  xl:overflow-auto gap-y-10">
 
-                    <Dialog.Close className="absolute top-2 right-3 md:top-6 md:right-6 xl:top-5 xl:right-5">
+                    <Dialog.Close className="absolute top-2 right-1 md:top-6 md:right-6 xl:top-5 xl:right-5">
                         <X/>
                     </Dialog.Close>
 
@@ -47,7 +47,7 @@ export default function Generations(props: Readonly<PokemonGeneration>){
                         <span className="text-center font-thin md:font-semibold">
                             Moves:
                         </span>
-                        <div className="">
+                        <div className="flex flex-col flex-wrap max-h-80 gap-x-2 gap-y-1">
                             {props.moves.map((move) => (
                                 <p key={move.id} className="text-center font-thin">
                                     {move.name}
@@ -58,7 +58,7 @@ export default function Generations(props: Readonly<PokemonGeneration>){
                         <span className="text-center font-thin md:font-semibold">
                             Version groups:
                         </span>
-                        <div className="">
+                        <div className="flex flex-col flex-wrap h-80 gap-x-2 gap-y-1">
                             {props.version_groups.map((group) => (
                                 <p key={group.id} className="text-center font-thin">
                                     {group.name}
